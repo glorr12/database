4c4e58158ed73f8a75078f26250d31168471daf4
glorr12 <igor.fishkov1991@gmail.com>
2026-06-21 09:26:30 +0100

    hw #20

---
+++ homework#20.py
@@ -0,0 +1,32 @@
+import re
+from pymongo import MongoClient
+
+{
+    'borough': 'Staten Island',
+    'name': re.compile(r"pizza(?i)")
+}
+
+result = client['sample_data']['restaurants'].find(
+  filter=filter
+)
+
+
+# ------------------------------------------
+
+result = client['sample_data']['restaurants'].aggregate([
+    {
+        '$project': {
+            '_id': 0,
+            'name': 1,
+            'grades.score': {
+                '$avg': '$grades.score'
+            }
+        }
+    }, {
+        '$sort': {
+            'grades.score': -1
+        }
+    }, {
+        '$limit': 5
+    }
+])
